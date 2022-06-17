19419318e6887fde396cdf246e06467e888c5c6c
SHOHAN <shohanurrahman2334@gmail.com>
2022-06-18 00:53:38 +0600

    blog page add

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+        <a class="navbar-brand p-2">
+            <h3>Blog</h3>
+        </a>
+        <div class="d-flex justify-content-end ">
+            @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                <a href="{{ url('/dashboard') }}"
+                    class="text-sm text-gray-700 dark:text-gray-500 underline btn btn-outline-info">Dashboard</a>
+                @else
+                <a href="{{ route('login') }}"
+                    class="text-sm text-gray-700 dark:text-gray-500 underline btn btn-outline-success">Log in</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}"
+                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline btn btn-outline-primary">Register</a>
+                @endif
+                @endauth
+            </div>
+            @endif
+        </div>
+        {{-- <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> --}}
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
